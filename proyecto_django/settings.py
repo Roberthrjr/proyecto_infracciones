1e329bef421f084e3c032011f5b0db580e54e832
Roberth Rios <tauroroberth00@gmail.com>
2024-05-18 23:39:27 -0500

    configuracion

--- proyecto_django/settings.py
+++ proyecto_django/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from os import environ
 from dotenv import load_dotenv
 from cloudinary import config
+import os
 
 # Cargando variables de entorno
 load_dotenv()
@@ -125,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
